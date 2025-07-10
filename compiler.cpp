94bb888d3ceccaaf8aea4cda61a8413c27f82cb5
Dakota <dakotacolinbell@gmail.com>
2025-07-10 23:22:11 +0000

    Basic syntaxical validator

--- compiler.cpp
+++ compiler.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <fstream>
 
+bool isSyntaxValid(int& argc, char*& argv) {
+    if (argc == 2) {
+        return true;
+    }
+
+    std::cout << "Invalid usage. Please follow the syntax: ./compiler [program.em]" << std::endl;
+    return false;
+}
+
 int main(int argc, char* argv) {
     // Confirm executable syntax is being followed : ./compiler [program.em]
+    if (!isSyntaxValid(argc, argv)) {
+        return EXIT_FAILURE;
+    }
 
     // Pull program.em file stream (FS)
 
@@ -15,5 +27,5 @@ int main(int argc, char* argv) {
     // Write to file
 
     // NOTE: Currently handling program assembling and linking externally.
-    return 0;
+    return EXIT_SUCCESS;
 }
