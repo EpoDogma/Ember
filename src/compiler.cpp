867fdaed53d0497fc1e550dec0094c730f904b1c
Dakota <dakotacolinbell@gmail.com>
2025-07-11 22:36:51 +0000

    Implemented first tokenization

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+
+enum class TokenType {
+    exit,
+    semicolon,
+    int_l
+};
+
+struct Token {
+    public:
+        TokenType type;
+        std::string data;
+        Token() {};
+        Token(TokenType type, std::string data) {
+            this->type = type;
+            this->data = data;
+        };
+        std::string getTypeString() {
+            std::string result;
+
+            if (type == TokenType::exit) {
+                result = "return";
+            }   else if (type == TokenType::semicolon) {
+                result = "semicolon";
+            }   else if (type == TokenType::int_l) {
+                result = "int_l";
+            }
+
+            return result;
+        }
+};
+
 
 bool isSyntaxValid(int&, char* argv[]);
+bool isint(std::string&);
+void extractUntilDiff(char&, std::string&, std::string&, bool);
 
 int main(int argc, char* argv[]) {
     // Confirm executable syntax is being followed : ./compiler [program.em]
@@ -17,11 +51,41 @@ int main(int argc, char* argv[]) {
     inS.resize(inFS.tellg());
     inFS.seekg(0, std::ios::beg);
     inFS.read(&inS[0], inS.size());
+    inFS.close();
 
     // Tokenize FS
-    
+    std::vector< Token > tokens;
+    std::string tokenString;
+    Token token;
+
+    while(inS.length() > 0) {
+        char c = inS.at(0);
+        tokenString = "";
+
+        if (std::isalpha(c)) {
+            extractUntilDiff(c, inS, tokenString, false);
+
+            if (tokenString == "return") {
+                tokens.push_back({TokenType::exit, tokenString});
+            }   else { // NOTE: Is not a keyword (variables or strings)
+                continue; // Not implemented yet.
+            }
+        }   else if (std::isdigit(c)) {
+            extractUntilDiff(c, inS, tokenString, true);
+            tokens.push_back({TokenType::int_l, tokenString});
+        }   else if (c == ';') {
+            tokenString.push_back(c);
+            tokens.push_back({TokenType::semicolon, tokenString});
+            inS.erase(inS.begin());
+        }   else if (c == ' ' || c == '\n') {
+            inS.erase(inS.begin());
+        }
+    }
 
     // Display tokenized FS
+    for (Token token : tokens) {
+        std::cout << "[" << token.getTypeString() << ", " << token.data << "]" << std::endl;
+    }
 
     // Translate into Assembly
 
@@ -40,5 +104,37 @@ bool isSyntaxValid(int& argc, char* argv[]) {
     return false;
 }
 
+bool isint(std::string& string) {
+    if (string.empty()) {
+        return false;
+    }
 
+    int start = 0;
+
+    if (string[0] == '-' || string[0] == '+') { // Explicit positie or negative number
+        if (string.length() == 1) { // Must have at least one digit
+            return false;
+        }
+
+        start++;
+    }
+
+    for (int i = start; i < string.length(); i++) {
+        if (!std::isdigit(string[i])) {
+            return false;
+        }
+    }
+
+    return true;  
+}
+
+void extractUntilDiff(char& c, std::string& inS, std::string& tokenString, bool isDigit) {
+    while (!inS.empty() && (isDigit) ? std::isdigit(c) : std::isalpha(c)) {
+        tokenString.push_back(c);
+        inS.erase(inS.begin());
+        if (!inS.empty()) {
+            c = inS.at(0);
+        }
+    }
+}
 
